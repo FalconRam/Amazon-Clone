1f709269458a2cdc9808ffe761b49bcabf78749b
FalconRam <ramvignesh974@gmail.com>
2021-07-27 23:10:32 +0530

    Home Page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMQpxLioom6j5yA1ti4BonZkLcPGXH0hk",
+  authDomain: "challenge-518a5.firebaseapp.com",
+  projectId: "challenge-518a5",
+  storageBucket: "challenge-518a5.appspot.com",
+  messagingSenderId: "116596002597",
+  appId: "1:116596002597:web:3e597e0b788efc0b7954cc",
+  measurementId: "G-GTJY7THP88",
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>;
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
